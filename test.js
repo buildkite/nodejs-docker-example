6401532400b0b0a55d5ca7499c39f039f961f295
zlseu-edu <zlseu.edu@gmail.com>
2020-03-20 17:20:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var assert = require('assert');
+var http = require('http');
 
-describe('String#split', function(){
-  it('should return an array', function(){
-    assert(Array.isArray('a,b,c'.split(',')));
-  });
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
